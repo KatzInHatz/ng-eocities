b8e0b487efcc505a5e2f53171e8b7d7c9f545995
John Doty <jrhdoty@gmail.com>
2014-09-27 16:51:10 -0700

    add a gulpfile to start setting up build system

---
+++ Gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+
+var paths = {
+  scripts: [],
+  source: [],
+  dist: './dist/'
+};
+
+gulp.task();
+//install gulp-load-plugins
+//make default task
+  //call concat task
+    //select source files
+    //.pipe($.concat("filename.js" //name of file with concat resources))
+    //.pipe(gulp.dest(<destination folder>))
+
+
+  //gulp-inject -- this is so dope
