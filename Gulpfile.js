6dda9ca6095c27b26ae9383fcd43cf01b1bf89fa
John Doty <jrhdoty@gmail.com>
2014-09-28 10:17:11 -0700

    add skeleton structure to gulp file

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,19 @@ var paths = {
   dist: './dist/'
 };
 
-gulp.task();
+gulp.task('lint', function(){});
+
+gulp.task('concat', function(){});
+
+gulp.task('minify', function(){});
+
+gulp.task('build', ['lint', 'concat', 'uglify']);
+
+gulp.task('watch', function(){
+  gulp.watch(paths.source, ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
 //install gulp-load-plugins
 //make default task
   //call concat task
