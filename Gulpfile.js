591682c9e6914cd5bcd7e9d2b333268682f5efcf
John Doty <jrhdoty@gmail.com>
2014-09-28 21:07:09 -0700

    add basic build tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,48 @@
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
 var paths = {
-  scripts: [],
-  source: [],
+  scripts: [
+    './src/**/*.js',
+    './src/ngEocities.js'
+  ],
+  source: [
+    './src/**/*.js',
+    './src/ngEocities.js'
+  ],
   dist: './dist/'
 };
 
 gulp.task('lint', function(){});
 
-gulp.task('concat', function(){});
+gulp.task('concat', function(){
+  return gulp.src(paths.scripts)
+    .pipe($.concat('ngEocities.js'))
+    .pipe(gulp.dest(paths.dist));
+});
 
-gulp.task('minify', function(){});
+gulp.task('minify', function(){
+  return gulp.src(paths.scripts)
+    .pipe($.concat('ngEocities.min.js'))
+    .pipe(gulp.dest(paths.dist));
+});
 
-gulp.task('build', ['lint', 'concat', 'uglify']);
+gulp.task('preMin', ['minify'], function(){
+  return gulp.src('./dist/ngEocities.min.js')
+    .pipe($.ngAnnotate())
+    .pipe(gulp.dest(paths.dist))
+    .pipe(notify({message: 'Min done'}));
+
+});
+
+gulp.task('uglify', ['preMin'], function(){
+  return gulp.src('./dist/ngEocities.min.js')
+    .pipe($.uglify())
+    .pipe(gulp.dest(paths.dist))
+    .pipe(notify({message: 'Build Done'}));
+});
+
+gulp.task('build', ['concat', 'uglify']);
 
 gulp.task('watch', function(){
   gulp.watch(paths.source, ['build']);
