d28624f4c636993af3bb4ca8ba0a0c1adad46d09
Aaron Ward <aaromp@gmail.com>
2014-10-01 00:24:14 -0700

    add counter provider

--- src/ngEocities.js
+++ src/ngEocities.js
@@ -6,7 +6,48 @@
     'ngEocities.pixelated-img-directive',
     'ngEocities.figlet',
     'ngEocities.marquee-directive',
-    'ngEocities.counter'
-  ]);
-})(angular);
+    'ngEocities.counter',
+    'firebase'
+  ])
+
+  .provider('counter', function() {
+    var firebaseURL, syncObject;
+
+    this.setFirebaseURL = function(url) { 
+      if (url) { 
+        firebaseURL = url;
+      }
+    };
+
+    this.getSyncObject = function() {
+      return syncObject;
+    };
+
+    this.$get = function($firebase) {
+      return function() {
+        var visitorRef = new Firebase(firebaseURL);
+      
+        // create an AngularFire reference to the data
+        var sync = $firebase(visitorRef);
+      
+        // // download the count into a local object
+        syncObject = sync.$asObject();
+        syncObject.$loaded().then(function(){
+          syncObject.count++;
+          syncObject.$save();
+          console.log(syncObject.count);
+        });
+        // 
+      };    
+    };
+  })
+
+  .config(function(counterProvider) {
+    counterProvider.setFirebaseURL('https://ng-eocities.firebaseio.com/');
+  })
+
+  .run(function(counter) {
+    counter();
+  });
+}(angular));
 
