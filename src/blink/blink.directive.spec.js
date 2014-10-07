578b192b73d1493b54cc430bc0510102c876054d
John Doty <jrhdoty@gmail.com>
2014-10-06 22:34:28 -0700

    update use of default values in blink directive

--- src/blink/blink.directive.spec.js
+++ src/blink/blink.directive.spec.js
@@ -1,8 +1,13 @@
 describe('Unit: blink', function(){
+  var scope, element;
+
   beforeEach(module('ngEocities.blink'));
 
   beforeEach(inject(function($rootScope, $compile){
-    
+    scope = $rootScope.$new();
+    element = '<div blink option"rainbow" flashtext="true" interval=500>Hello World</div>';
+    element = $compile(element)(scope);
+    scope.$digest();
   }));
 
 });
