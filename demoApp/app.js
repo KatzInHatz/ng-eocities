ff95bbc4f79b75c9aea4268b0618c7a78e4e5fe8
zzmp <zmp@umich.edu>
2014-10-07 01:46:40 -0700

    Added jukebox to demo

--- demoApp/app.js
+++ demoApp/app.js
@@ -5,7 +5,7 @@ app.config(function(counterProvider, figlifyProvider) {
   figlifyProvider.setFontsRoute('/figlet/fonts/');
 });
 
-app.controller('myController', function($scope, $timeout) {
+app.controller('myController', function($scope, $timeout, $sce) {
   var toggle = false;
   $scope.fig = 'type something';
   $scope.aaron = 'https://media.licdn.com/media/p/5/005/06a/07c/0ee6549.jpg';
@@ -13,6 +13,10 @@ app.controller('myController', function($scope, $timeout) {
   $scope.kat = 'http://petitecurie.com/wp-content/uploads/2014/03/cat8.jpg';
   $scope.link = 'http://occupyfun.com/media/videos/2/Cats-Transforming-into-Croissants.jpg';
   $scope.pixelation = 4;
+  $scope.jukebox = {
+    src: $sce.trustAsResourceUrl("http://uhmp3.com/user-mp3-to/8_scream-and-shout-william-and-britney-spears.mp3"),
+    volume: 1
+  };
   $scope.action = function() {
     console.log(toggle);
     $scope.link = toggle ? 'http://www.schoolforthedogs.com/wp-content/uploads/2012/10/Screen-Shot-2014-02-06-at-7.15.27-PM-300x200.png' : 'http://occupyfun.com/media/videos/2/Cats-Transforming-into-Croissants.jpg';
