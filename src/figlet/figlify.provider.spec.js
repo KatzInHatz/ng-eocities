ea484bfa90b1db9a0cbf2180b02186373a1b95ce
John Doty <jrhdoty@gmail.com>
2014-10-06 19:53:31 -0700

    add initial test and setup for figlify.provider module

--- src/figlet/figlify.provider.spec.js
+++ src/figlet/figlify.provider.spec.js
@@ -1,7 +1,50 @@
-describe('Testing Tests', function(){
+describe('Unit: Figlify', function(){
+  //load module we're testing
+  beforeEach(function(){
+    //configure provider
+    angular.module('ngEocities.figlet')
+      .config(function(figlifyProvider){
+        figlifyProvider.setFontsRoute('/figlify/fonts/');
+      });
 
-  it('true should equal true', function(){
-    expect(true).to.equal(true);
+    //load module
+    module('ngEocities.figlet');
   });
 
+  var figlify, $httpBackend;
+
+  beforeEach(inject(function(_figlify_, _$httpBackend_){
+    //inject provider and mockup $http service
+    figlify = _figlify_;
+    $httpBackend = _$httpBackend_;
+
+    //expect request to get font
+    $httpBackend.when('GET', '/figlify/fonts/alligator.flf')
+      .respond('hello world');
+
+  }));
+
+  var text; 
+
+  beforeEach(function(){
+    text = 'hello';
+  });
+
+  afterEach(function(){
+    $httpBackend.flush();
+  });
+
+
+  it('should correctly request a font', function(){
+    figlify.figlify(text, 'alligator', function(str){
+    });
+  });
 });
+
+
+
+
+
+
+
+
